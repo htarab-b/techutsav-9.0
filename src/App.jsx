6cbd40b90e1798546b18ce7d591928b417efad72
santhoshsj-dev <santhoshsekar2903@gmail.com>
2024-03-01 06:46:55 +0530

    event page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
-import Home from './components/Home';
+import HomePage from './pages/HomePage';
+import EventsPage from './pages/EventsPage';
 import './App.css';
 
 const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Home />} />
-  )
+  createRoutesFromElements([
+    <Route path="/" element={<HomePage />} />,
+    <Route path="/events" element={<EventsPage />} />
+  ])
 );
 
 function App() {
